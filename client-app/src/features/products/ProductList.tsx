9341d3f5244ad44c5882edb7a4c54de3a4081f52
Michal Fiala <michfiala.it@gmail.com>
2021-07-23 22:31:39 +0200

    Setting up and touching backend API

--- client-app/src/features/products/ProductList.tsx
+++ client-app/src/features/products/ProductList.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from 'semantic-ui-react';
+import agent from '../../app/api/agent';
+import { Product } from '../../app/models/product';
 import ProductListItem from './ProductListItem';
 
 export default function ProductList()
 {
+     const [products, setProducts] = useState<Product[]>([]);
+
+     useEffect(() => {
+          agent.Products.list().then(value => setProducts(value));
+
+     }, []);
+
      return (
           <>
                <Header content='Product List' />
                
+               {
+                    products.map(p => (
+                         <Header key={p.id} content={p.name}/>
+                    ))
+               }
                <ProductListItem />
           </>
      )
